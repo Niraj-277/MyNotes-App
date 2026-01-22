bf168f276f7a21b8d41b2c3d7b99f442ca47d8c9
Niraj <nirajrampal217@gmail.com>
2026-01-22 23:17:52 +0530

    server and database setup complete

---
+++ config/db.js
@@ -0,0 +1,14 @@
+ const mongoose=require('mongoose')
+
+const connectDb=async()=>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`connected to mongodb${conn.connection.host}`)
+
+    }catch(error){
+        console.error(`Error: ${error.message}`)
+        process.exit(1)
+    } 
+}
+
+module.exports = connectDb;
--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express= require('express')
 const app= express()
+const dotenv= require('dotenv') 
 
-app.get('/hello',()=>{
-    console.log("hello world")
+//!1. Load config first
+dotenv.config();
+
+//! 2. connect to database
+const connectDb = require('./config/db')
+connectDb();
+
+//! Middleware 
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+    res.send('api is running...')
 })
 
-const PORT=3000
+const PORT=process.env.PORT || 5000;
 app.listen(PORT,()=>{
-    console.log("server running at 3000")
+    console.log(`server is running at ${PORT}`);
 })
