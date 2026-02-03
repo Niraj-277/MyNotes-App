526052de48e7229deead201742c9fc2bb34ecbff
Niraj <nirajrampal217@gmail.com>
2026-02-03 11:29:20 +0530

    fixed issues

--- utils/validators.js
+++ utils/validators.js
@@ -6,7 +6,7 @@ const Joi= require("joi");
 const registerSchema=Joi.object({
     name:Joi.string().min(3).required(),
     email:Joi.string().email().required(),
-    password:Joi.string().min(6).required
+    password:Joi.string().min(6).required()
 });
 
 
@@ -15,7 +15,7 @@ const registerSchema=Joi.object({
 
 const loginSchema=Joi.object({
     email:Joi.string().email().required(),
-    password:Joi.string().required,
+    password:Joi.string().required(),
 });
 
 //3. Create note validation
